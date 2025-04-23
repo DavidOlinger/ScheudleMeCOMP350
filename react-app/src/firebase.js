74c43b3dfea8d62f3d039bee12aa4ad052eddc68
JdubsXVIII <126510635+JdubsXVIII@users.noreply.github.com>
2025-04-23 16:03:47 -0400

    Set up react and firebase stuff

---
+++ react-app/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3KhyIytYz1McR0bRzqbViwJ7TwMgQVJ4",
+  authDomain: "scheudleme.firebaseapp.com",
+  projectId: "scheudleme",
+  storageBucket: "scheudleme.firebasestorage.app",
+  messagingSenderId: "970637457230",
+  appId: "1:970637457230:web:a408ae9f85e61512c5101f",
+  measurementId: "G-T5C4EPYD6H"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
